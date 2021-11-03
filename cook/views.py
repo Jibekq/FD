7d89468a84e65240d8602b735f5bc41818f74cb4
Jibekq <jibeknasipbek@gmail.com>
2021-11-04 00:39:18 +0200

    added

--- cook/views.py
+++ cook/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from cook.models import Post
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs("slug"))
+
+
+        
+def home(request):
+    return render(request, 'base.html')
